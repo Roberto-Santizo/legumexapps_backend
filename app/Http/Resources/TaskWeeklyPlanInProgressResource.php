1dd2c3c23e07968e1fe9e403dcc17d156161c9e6
Jose Roberto Acan Santizo <auxtic04@legumexadt.net>
2025-02-15 23:22:56 -0600

    refactor(agricola dashboard): actualiza la manera de validar al usuario por medio del token y no un param

--- app/Http/Resources/TaskWeeklyPlanInProgressResource.php
+++ app/Http/Resources/TaskWeeklyPlanInProgressResource.php
@@ -16,6 +16,7 @@ class TaskWeeklyPlanInProgressResource extends JsonResource
     {
         $assigned_employees = $this->employees->count();
         $paused = $this->closures->last();
+        $has_insumos = $this->insumos->count() > 0 ? true : false;
         return [
             'id' => strval($this->id),
             'task' => $this->task->name,
@@ -24,7 +25,8 @@ class TaskWeeklyPlanInProgressResource extends JsonResource
             'week' => $this->plan->week,
             'assigned_employees' => $assigned_employees,
             'total_employees' => $this->workers_quantity,
-            'paused' => ($paused && !$paused->end_date) ? true : false
+            'paused' => ($paused && !$paused->end_date) ? true : false,
+            'has_insumos' => $has_insumos
         ];
     }
 }
