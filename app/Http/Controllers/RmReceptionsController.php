44a02d2e9d8902aee8f04352db7e9b3d9ae924fa
Roberto Santizo <soportetecnico.tejar@legumex.net>
2025-12-04 09:31:39 -0600

    bugfix

--- app/Http/Controllers/RmReceptionsController.php
+++ app/Http/Controllers/RmReceptionsController.php
@@ -96,20 +96,20 @@ class RmReceptionsController extends Controller
         $signature3 = $data['inspector_signature'];
 
         try {
-            // list(, $signature1) = explode(',', $signature1);
-            // $signature1 = base64_decode($signature1);
-            // $filename1 = 'signatures/' . uniqid() . '.png';
-            // Storage::disk('s3')->put($filename1, $signature1, 'public');
+            list(, $signature1) = explode(',', $signature1);
+            $signature1 = base64_decode($signature1);
+            $filename1 = 'signatures/' . uniqid() . '.png';
+            Storage::disk('s3')->put($filename1, $signature1, 'public');
 
-            // list(, $signature2) = explode(',', $signature2);
-            // $signature2 = base64_decode($signature2);
-            // $filename2 = 'signatures/' . uniqid() . '.png';
-            // Storage::disk('s3')->put($filename2, $signature2, 'public');
+            list(, $signature2) = explode(',', $signature2);
+            $signature2 = base64_decode($signature2);
+            $filename2 = 'signatures/' . uniqid() . '.png';
+            Storage::disk('s3')->put($filename2, $signature2, 'public');
 
-            // list(, $signature3) = explode(',', $signature3);
-            // $signature3 = base64_decode($signature3);
-            // $filename3 = 'signatures/' . uniqid() . '.png';
-            // Storage::disk('s3')->put($filename3, $signature3, 'public');
+            list(, $signature3) = explode(',', $signature3);
+            $signature3 = base64_decode($signature3);
+            $filename3 = 'signatures/' . uniqid() . '.png';
+            Storage::disk('s3')->put($filename3, $signature3, 'public');
 
             $payload = JWTAuth::getPayload();
             $id = $payload->get('id');
@@ -166,9 +166,9 @@ class RmReceptionsController extends Controller
                 'weight_baskets' => round(($basket->weight * $data['total_baskets']), 2),
                 'quality_percentage' => $data['quality_percentage'],
                 'basket_id' => $basket->id,
-                'driver_signature' => 'FILENAME.PNG',
-                'prod_signature' => 'FILENAME.PNG',
-                'inspector_signature' => 'FILENAME.PNG',
+                'driver_signature' => $filename1,
+                'prod_signature' => $filename2,
+                'inspector_signature' => $filename3,
                 'plate_id' => $plate->id,
                 'cdp_id' => $productor_cdp->id,
                 'carrier_id' => $carrier->id,
